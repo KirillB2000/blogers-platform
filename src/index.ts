7d7b788cd5e9425d4330dc4bd8568dfb1abfd343
Kirill_Byckov <kiril.byckov.2000@gmail.com>
2026-07-07 23:11:40 +0300

    'add-basic-authorization'

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import setupApp from './setup-app';
+import { SETTINGS } from './settings/config';
 
 export const app = express()
 export default setupApp(app)
 
-const PORT = process.env.PORT || 5002
+const PORT = SETTINGS.PORT
 
 app.listen(PORT, () => {
     console.log(`Serever is running on http://localhost:${PORT}`);
