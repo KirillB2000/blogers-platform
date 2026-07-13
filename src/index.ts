4fdaaf4163eaa5eca9e4cafe6bc500ed99a81f21
Kirill_Byckov <kiril.byckov.2000@gmail.com>
2026-07-13 19:35:51 +0300

    'fix-connecting-to-db'

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
 import express from "express";
 import setupApp from "./setup-app";
 import { SETTINGS } from "./settings/config";
+import { runDB } from "./db/mongo.db";
+import { settings } from "node:cluster";
 
 const bootstrap = async () => {
   const app = express();
   setupApp(app);
   
   const PORT = SETTINGS.PORT;
+
+  await runDB(SETTINGS.MONGO_URL)
   
   if (!SETTINGS.VERCEL) {
     app.listen(PORT, () => {
