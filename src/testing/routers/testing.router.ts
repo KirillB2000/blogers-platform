61ec5c4cb6c3d1ca6b4a21d79a2474a1780f0f3a
Kirill_Byckov <kiril.byckov.2000@gmail.com>
2026-07-27 03:38:39 +0300

    fix-for-tests

--- src/testing/routers/testing.router.ts
+++ src/testing/routers/testing.router.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import { TESTING_ROUTES } from "../constants/testing.paths";
 import { testingDeleteAllDataHandler } from "./handlers/testingDeleteAllData.handler";
+import { catchAsync } from "../../core/helpers/catchAsync.helper";
 
 export const testingRouter = Router({});
 
-testingRouter.delete(TESTING_ROUTES.ALL_DATA, testingDeleteAllDataHandler);
+testingRouter.delete(TESTING_ROUTES.ALL_DATA, catchAsync(testingDeleteAllDataHandler));
