cbde81d8b68a0d43eb3835db62c94a77c2766d3c
Kirill_Byckov <kiril.byckov.2000@gmail.com>
2026-08-04 17:05:05 +0300

    add-auth-endpont-and-tests-for-it

---
+++ __tests__/e2e/auth/auth-body-validation.e2e.spec.ts
@@ -0,0 +1,45 @@
+import { AUTH_PATH, AUTH_ROUTING } from "../../../src/auth/constants/auth.paths";
+import { LoginInputModel } from "../../../src/auth/dto/authInputModel";
+import { httpStatuses } from "../../../src/core/types/http-statuses";
+import { usersCollection } from "../../../src/db/collections";
+import { runDB, stopDb } from "../../../src/db/mongo.db";
+import { SETTINGS } from "../../../src/settings/config";
+import setupApp from "../../../src/setup-app";
+import { clearDb } from "../../utils/clearDb";
+import express from 'express'
+import request from "supertest";
+
+describe("Auth API body validation check", () => {
+    const app = express();
+    setupApp(app);
+
+    const correctAuthDto: LoginInputModel = {
+        loginOrEmail: 'someLoginOrEmail',
+        password: 'somePassord'
+    }
+
+    beforeAll(async () => {
+        await runDB(SETTINGS.MONGO_URL)
+        await usersCollection.deleteMany({})
+    });
+    
+    afterAll(async () => {
+        await clearDb(app);
+        await stopDb()
+    })
+
+    it('Should not log in user with incorrect data', async () => {
+        const incorrectInputData: LoginInputModel = {
+            ...correctAuthDto,
+            loginOrEmail: '',
+            password: ''
+        }
+
+        const response = await request(app)
+            .post(`${AUTH_PATH}${AUTH_ROUTING.LOGIN}`)
+            .send(incorrectInputData)
+            .expect(httpStatuses.BadRequest)
+
+        expect(response.body.errorsMessages).toHaveLength(2)
+    })
+})
--- src/setup-app.ts
+++ src/setup-app.ts
@@ -1,5 +1,5 @@
 import express, { Express } from "express";
-import { blogsRouter } from "./blogs/routes/blogs.routers";
+import { blogsRouter } from "./blogs/routes/blogs.router";
 import { postsRouter } from "./posts/routes/posts.router";
 import { BLOGS_PATH } from "./blogs/constants/blogs.paths";
 import { POSTS_PATH } from "./posts/constants/posts.paths";
@@ -8,6 +8,8 @@ import { testingRouter } from "./testing/routers/testing.router";
 import { errorsHandler } from "./core/exceptions/error.handler";
 import { USERS_PATH } from "./users/constants/users.paths";
 import { userRouter } from "./users/routes/users.router";
+import { AUTH_PATH } from "./auth/constants/auth.paths";
+import { authRouter } from "./auth/routes/auth.router";
 
 const setupApp = (app: Express) => {
   app.use(express.json());
@@ -16,6 +18,7 @@ const setupApp = (app: Express) => {
   app.use(POSTS_PATH, postsRouter);
   app.use(TESTING_PATH, testingRouter);
   app.use(USERS_PATH, userRouter)
+  app.use(AUTH_PATH, authRouter)
 
   app.use(errorsHandler)
 
