32299c5023218f2261047bf0532fc052a9e4057a
Kirill_Byckov <kiril.byckov.2000@gmail.com>
2026-08-04 19:39:45 +0300

    fix-for-tests-3

--- src/testing/routers/handlers/testingDeleteAllData.handler.ts
+++ src/testing/routers/handlers/testingDeleteAllData.handler.ts
@@ -1,10 +1,11 @@
 import { Request, Response } from "express";
 import { httpStatuses } from "../../../core/types/http-statuses";
-import { blogsCollection, postsCollection } from "../../../db/collections";
+import { blogsCollection, postsCollection, usersCollection } from "../../../db/collections";
 
 export const testingDeleteAllDataHandler = async (req: Request, res: Response) => {
   await postsCollection.deleteMany({})
   await blogsCollection.deleteMany({})
+  await usersCollection.deleteMany({})
 
   res.sendStatus(httpStatuses.NoContent)
 };
